ce24de1e033859c4b0f40e2c5f9a83e5f9ece6b8
DD <471313931@qq.com>
2020-01-30 13:22:57 +0800

    finish 003

---
+++ 003-Java的基本程序设计结构/example-3.8.3/bigIntergereg.java
@@ -0,0 +1,11 @@
+import java.math.*;
+import java.util.*;
+
+public class bigIntergereg {
+
+    public static void main(String[] args) {
+        int a = 2;
+        BigInteger b = BigInteger.valueOf(a);
+        System.out.println(b);
+    }
+}
